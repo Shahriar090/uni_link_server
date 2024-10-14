40df53db1bbac4b507e93619c7b9e6d4f3ba0434
Shahriar <shahriarh655@gmail.com>
2024-10-14 23:11:59 +0600

    Created type for student

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,43 @@
+import { Schema, model, connect } from 'mongoose';
+
+// sub types
+
+export type TGuardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+export type TUserName = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+
+export type TLocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+// main type
+export type TStudent = {
+  id: string;
+  name: TUserName;
+  email: string;
+  gender: 'Male' | 'Female';
+  dateOfBirth: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: TGuardian;
+  localGuardian: TLocalGuardian;
+  profileImage?: string;
+  isActive: 'Active' | 'Blocked';
+};
