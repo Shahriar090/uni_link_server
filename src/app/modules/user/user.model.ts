90f3533608078a63f47c198bb5c552066fb30079
Shahriar <shahriarh655@gmail.com>
2024-10-18 23:37:36 +0600

    Implemented user schema and model

---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['Admin', 'Faculty', 'Student'],
+    },
+    status: {
+      type: String,
+      enum: ['In-Progress', 'Blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true },
+);
+
+// user model
+
+export const User = model<TUser>('User', userSchema);
