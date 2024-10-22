a6c5b61973c5831472f57bf2d383ee8316b2a53d
Shahriar <shahriarh655@gmail.com>
2024-10-23 00:20:02 +0600

    Implemented validation in the academic semester service to check if the semester name and code match before creating a semester.

--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,7 +1,13 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
 
 const createAcademicSemesterIntoDb = async (payload: TAcademicSemester) => {
+  // checking if the semester name and code are matching together or not
+
+  if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
+    throw new Error('Invalid Semester Code');
+  }
   const result = await AcademicSemester.create(payload);
   return result;
 };
