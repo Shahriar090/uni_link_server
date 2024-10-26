ae6f19a2a005b70ce0199a0b62c399e391ec1598
Shahriar <shahriarh655@gmail.com>
2024-10-26 21:55:17 +0600

    Modified global routes and academic faculty validation schemas

--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,13 +1,17 @@
 import z from 'zod';
 const createAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty Name Must Be An String',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty Name Must Be An String',
+    }),
   }),
 });
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z.string({
-    invalid_type_error: 'Academic Faculty Name Must Be An String',
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Faculty Name Must Be An String',
+    }),
   }),
 });
 
--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -20,7 +20,7 @@ const moduleRoutes = [
     route: academicSemesterRoutes,
   },
   {
-    path: '/academic-faculty',
+    path: '/academic-faculties',
     route: academicFacultyRoutes,
   },
 ];
