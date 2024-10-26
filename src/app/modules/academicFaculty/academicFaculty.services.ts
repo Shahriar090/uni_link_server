609ad47cf409f1049f2c902d5b031f7702c9120f
Shahriar <shahriarh655@gmail.com>
2024-10-26 19:49:25 +0600

    Implemented create, get all and get single academic faculty services functionality

---
+++ src/app/modules/academicFaculty/academicFaculty.services.ts
@@ -0,0 +1,25 @@
+import { TAcademicFaculty } from './academicFaculty.interface';
+import { AcademicFaculty } from './academicFaculty.model';
+
+// create academic faculty into db
+const createAcademicFacultyIntoDb = async (payload: TAcademicFaculty) => {
+  const result = await AcademicFaculty.create(payload);
+  return result;
+};
+
+// get all academic faculties into db
+const getAllAcademicFacultiesFromDb = async () => {
+  const result = await AcademicFaculty.find();
+  return result;
+};
+
+// get a single academic faculty from db
+const getSingleAcademicFacultyFromDb = async (id: string) => {
+  const result = await AcademicFaculty.findById(id);
+  return result;
+};
+export const academicFacultyServices = {
+  createAcademicFacultyIntoDb,
+  getAllAcademicFacultiesFromDb,
+  getSingleAcademicFacultyFromDb,
+};
