99a9c6d423621fdfb802fcc797a03c58b9abf4bb
Shahriar <shahriarh655@gmail.com>
2024-10-27 19:19:19 +0600

    Implemented validation for academic department with zod

---
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -0,0 +1,36 @@
+import z from 'zod';
+
+const createAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z.string({
+      invalid_type_error: 'Academic Department Must Be An String',
+      required_error: 'Academic Department Is Required',
+    }),
+    academicFaculty: z.string({
+      invalid_type_error: 'Academic Faculty Must Be An String',
+      required_error: 'Academic Faculty Is Required',
+    }),
+  }),
+});
+
+const updateAcademicDepartmentValidationSchema = z.object({
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic Department Must Be An String',
+        required_error: 'Academic Department Is Required',
+      })
+      .optional(),
+    academicFaculty: z
+      .string({
+        invalid_type_error: 'Academic Faculty Must Be An String',
+        required_error: 'Academic Faculty Is Required',
+      })
+      .optional(),
+  }),
+});
+
+export const academicDepartmentValidations = {
+  createAcademicDepartmentValidationSchema,
+  updateAcademicDepartmentValidationSchema,
+};
