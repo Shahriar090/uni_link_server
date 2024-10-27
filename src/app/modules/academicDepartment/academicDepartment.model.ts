f8bfe8c09cf2723b8604ab4c3ac0e6579653b524
Shahriar <shahriarh655@gmail.com>
2024-10-27 21:53:28 +0600

    Implemented validation using pre hook middleware in the academic department model to prevent the duplication of academic departments and to prevent updates to a deleted document

--- src/app/modules/academicDepartment/academicDepartment.model.ts
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -16,6 +16,25 @@ const academicDepartmentSchema = new Schema<TAcademicDepartment>(
   { timestamps: true },
 );
 
+// checking if the department already exist or not into db before creating.
+academicDepartmentSchema.pre('save', async function (next) {
+  const isDepartmentExist = await AcademicDepartment.findOne({
+    name: this.name,
+  });
+
+  if (isDepartmentExist) throw new Error('This Department Already Exist.!');
+  next();
+});
+
+// to not update deleted department
+academicDepartmentSchema.pre('findOneAndUpdate', async function (next) {
+  const query = this.getQuery();
+  const isDepartmentExist = await AcademicDepartment.findOne(query);
+
+  if (!isDepartmentExist) throw new Error('This Department Does Not Exist.!');
+  next();
+});
+
 export const AcademicDepartment = model<TAcademicDepartment>(
   'AcademicDepartment',
   academicDepartmentSchema,
