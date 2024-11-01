c2f2b80236b33d5b919598275319569253b67c2d
Shahriar <shahriarh655@gmail.com>
2024-11-01 10:23:58 +0600

    Formating global error handler to detect error pattern

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,30 @@
 import { ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
 export const globalErrorHandler: ErrorRequestHandler = (err, _, res, next) => {
   console.error(err.stack);
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something Went Wrong';
 
-  res.status(err.statusCode || 500).json({
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something Went Wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    (statusCode = 400), (message = 'I am zod error');
+  }
+  res.status(statusCode).json({
     success: false,
-    message: err.message || 'Something Went Wrong',
+    message,
+    errorSource,
     error: err,
   });
   next();
