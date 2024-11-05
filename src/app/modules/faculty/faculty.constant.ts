1bbcb3a865364b9c1671bd99a012dcd9b7471f55
Shahriar <shahriarh655@gmail.com>
2024-11-05 18:49:13 +0600

    Implemented faculty schema with virtuals

---
+++ src/app/modules/faculty/faculty.constant.ts
@@ -0,0 +1,13 @@
+import { TBloodGroup, TGender } from './faculty.interface';
+
+export const Gender: TGender[] = ['Male', 'Female', 'Others'];
+export const BloodGroup: TBloodGroup[] = [
+  'A+',
+  'A-',
+  'AB+',
+  'AB-',
+  'B+',
+  'B-',
+  'O+',
+  'O-',
+];
---
+++ src/app/modules/faculty/faculty.model.ts
@@ -0,0 +1,98 @@
+import { Schema } from 'mongoose';
+import { FacultyModel, TFaculty, TUserName } from './faculty.interface';
+import { BloodGroup, Gender } from './faculty.constant';
+
+const userNameSchema = new Schema<TUserName>({
+  firstName: {
+    type: String,
+    required: [true, 'First Name Is Required'],
+    trim: true,
+    maxlength: [20, 'Name Cannot Be More Than 20 Characters'],
+  },
+  middleName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: [true, 'Last Name Is Required'],
+    trim: true,
+    maxlength: [20, 'Name Cannot Be More Than 20 Characters'],
+  },
+});
+
+const facultySchema = new Schema<TFaculty, FacultyModel>(
+  {
+    id: {
+      type: String,
+      required: [true, 'ID Is Required'],
+      unique: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      required: [true, 'User ID Required'],
+      unique: true,
+      ref: 'User',
+    },
+    designation: {
+      type: String,
+      required: [true, 'Designation Is Required'],
+    },
+    name: {
+      type: userNameSchema,
+      required: [true, 'Name Is Required'],
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: Gender,
+        message: '{VALUE} Is Not A Valid Gender',
+      },
+      required: [true, 'Gender Is Required'],
+    },
+    dateOfBirth: { type: Date },
+    email: {
+      type: String,
+      required: [true, 'Email Is Required'],
+      unique: true,
+    },
+    contactNo: { type: String, required: [true, 'Contact No Is Required'] },
+    emergencyContactNo: {
+      type: String,
+      required: [true, 'Emergency Contact No Is Required'],
+    },
+    bloodGroup: {
+      type: String,
+      enum: {
+        values: BloodGroup,
+        message: '{VALUE} Is Not A Valid Blood Group',
+      },
+    },
+    presentAddress: {
+      type: String,
+      required: [true, 'Present Address Is Required'],
+    },
+    permanentAddress: {
+      type: String,
+      required: [true, 'Permanent Address Id Required'],
+    },
+    profileImg: { type: String },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    isDeleted: { type: Boolean, default: false },
+  },
+  { timestamps: true, toJSON: { virtuals: true } },
+);
+
+// generating full name
+facultySchema.virtual('fullName').get(function () {
+  return (
+    this?.name?.firstName +
+    '' +
+    this?.name?.middleName +
+    '' +
+    this?.name?.lastName
+  );
+});
