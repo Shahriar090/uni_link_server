33f8f02e485eb3e86b87fc2bccc13254ee808322
Shahriar <shahriarh655@gmail.com>
2024-11-07 19:43:22 +0600

    Successfully implemented update logics for faculty.

--- src/app/modules/faculty/faculty.routes.ts
+++ src/app/modules/faculty/faculty.routes.ts
@@ -1,7 +1,18 @@
 import express from 'express';
 import { facultyControllers } from './faculty.controllers';
+import { validateRequest } from '../../middlewares/validateRequest';
+import { facultyValidations } from './faculty.validation';
 const facultyRouter = express.Router();
+// get
 facultyRouter.route('/').get(facultyControllers.getAllFaculties);
+// get single
 facultyRouter.route('/:id').get(facultyControllers.getSingleFaculty);
+// update faculty
+facultyRouter
+  .route('/:id')
+  .patch(
+    validateRequest(facultyValidations.updateFacultyValidationSchema),
+    facultyControllers.updateFaculty,
+  );
 
 export const facultyRoutes = facultyRouter;
