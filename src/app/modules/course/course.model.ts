e1772e71b5d9491ab8607167a00c8fa17fbe42f9
Shahriar <shahriarh655@gmail.com>
2024-11-16 11:53:59 +0600

    Implemented the isDeleted field in the course interface, model, and validations

--- src/app/modules/course/course.model.ts
+++ src/app/modules/course/course.model.ts
@@ -12,27 +12,31 @@ const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourse>({
   },
 });
 
-const courseSchema = new Schema<TCourse>({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true,
+const courseSchema = new Schema<TCourse>(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+    },
+    prefix: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    code: {
+      type: Number,
+      required: true,
+    },
+    credits: {
+      type: Number,
+      required: true,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+    isDeleted: { type: Boolean, default: false },
   },
-  prefix: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  code: {
-    type: Number,
-    required: true,
-  },
-  credits: {
-    type: Number,
-    required: true,
-  },
-  preRequisiteCourses: [preRequisiteCoursesSchema],
-});
+  { timestamps: true },
+);
 
 export const Course = model<TCourse>('Course', courseSchema);
