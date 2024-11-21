49be135c7e4c18a8d2d27bab842e1c0195372ecf
Shahriar <shahriarh655@gmail.com>
2024-11-21 21:08:39 +0600

    Updated semester registration validations file. Implemented validations for creating semester registration.

--- src/app/modules/semesterRegistration/semesterRegistration.validations.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validations.ts
@@ -1,7 +1,19 @@
 import { z } from 'zod';
+import { semesterRegistrationConstant } from './semesterRegistration.constants';
 
 const createSemesterRegistrationValidationSchema = z.object({
-  body: z.object({}),
+  body: z.object({
+    academicSemester: z.object({
+      academicSemester: z.string(),
+      status: z.enum([
+        ...(semesterRegistrationConstant as [string, ...string[]]),
+      ]),
+      startDate: z.string().datetime(),
+      endDate: z.string().datetime(),
+      minCredit: z.number(),
+      maxCredit: z.number(),
+    }),
+  }),
 });
 
 export const semesterRegistrationValidations = {
