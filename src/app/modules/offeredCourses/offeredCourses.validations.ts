9a3cae186d484f0d3eaf55f21dffb4c33b9b0758
Shahriar <shahriarh655@gmail.com>
2024-12-06 19:17:07 +0600

    Implemented validations for offered courses.

---
+++ src/app/modules/offeredCourses/offeredCourses.validations.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+import { Days } from './offeredCourses.constants';
+
+const createOfferedCoursesValidationSchema = z.object({
+  body: z.object({
+    semesterRegistration: z.string(),
+    academicSemester: z.string(),
+    academicFaculty: z.string(),
+    academicDepartment: z.string(),
+    course: z.string(),
+    faculty: z.string(),
+    section: z.number(),
+    maxCapacity: z.number(),
+    days: z.enum([...Days] as [string, ...string[]]),
+    startTime: z.string(),
+    endTime: z.string(),
+  }),
+});
+
+const updateOfferedCoursesValidationSchema = z.object({
+  body: z.object({
+    faculty: z.string().optional(),
+    maxCapacity: z.number().optional(),
+    days: z.enum([...Days] as [string, ...string[]]).optional(),
+    startTime: z.string().optional(),
+    endTime: z.string().optional(),
+  }),
+});
+
+export const offeredCoursesValidations = {
+  createOfferedCoursesValidationSchema,
+  updateOfferedCoursesValidationSchema,
+};
