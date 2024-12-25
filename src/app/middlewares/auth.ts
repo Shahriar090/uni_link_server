0b11fa5bc0eb52385a82bf22c531933532c69080
Shahriar <shahriarh655@gmail.com>
2024-12-25 12:40:23 +0600

    Updated auth middleware. Implemented logics to verify the token and then assigned it to request object as user if the token is valid.

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -2,10 +2,14 @@ import { NextFunction, Request, Response } from 'express';
 import { catchAsync } from '../utils/catchAsync';
 import httpStatus from 'http-status-codes';
 import AppError from '../errors/appError';
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import config from '../config';
 
 const auth = () => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req?.headers?.authorization;
+
+    // check if token is not provided
     if (!token) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
@@ -13,6 +17,18 @@ const auth = () => {
         '',
       );
     }
+
+    // check if token is valid
+    jwt.verify(token, config?.access_token_secret as string, (err, decoded) => {
+      if (err) {
+        throw new AppError(
+          httpStatus.UNAUTHORIZED,
+          'You Are Not Authorized! Your Token is Invalid/Expired',
+          '',
+        );
+      }
+      req.user = decoded as JwtPayload;
+    });
     next();
   });
 };
