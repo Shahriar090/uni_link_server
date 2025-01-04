e7c7243f80ab89b6378d7578f5ba9d0594b79401
Shahriar <shahriarh655@gmail.com>
2025-01-04 21:20:17 +0600

    Updated enrolled course services. Implemented validations and an aggregation pipeline to count total credits, and finally allowed the student to enroll in the course if all necessary validations were successfully fulfilled.

--- src/app/modules/enrolledCourse/enrolledCourse.services.ts
+++ src/app/modules/enrolledCourse/enrolledCourse.services.ts
@@ -5,6 +5,8 @@ import { TEnrolledCourse } from './enrolledCourse.interface';
 import httpStatusCodes from 'http-status-codes';
 import EnrolledCourse from './enrolledCourse.model';
 import mongoose from 'mongoose';
+import { SemesterRegistration } from '../semesterRegistration/semesterRegistration.model';
+import { Course } from '../course/course.model';
 
 const createEnrolledCourseIntoDb = async (
   userId: string,
@@ -25,7 +27,7 @@ const createEnrolledCourseIntoDb = async (
   if (isOfferedCourseExists?.maxCapacity <= 0) {
     throw new AppError(httpStatusCodes.CONFLICT, 'Room Capacity is Full.!', '');
   }
-  const student = await Student.findOne({ id: userId }).select('id');
+  const student = await Student.findOne({ id: userId }, { _id: 1 });
   if (!student) {
     throw new AppError(httpStatusCodes.NOT_FOUND, 'Student Not Found', '');
   }
@@ -41,6 +43,62 @@ const createEnrolledCourseIntoDb = async (
     throw new AppError(
       httpStatusCodes.CONFLICT,
       'Student Already Enrolled',
+      '',
+    );
+  }
+
+  // check total credit exceeds max credit
+  const semesterRegistration = await SemesterRegistration.findById(
+    isOfferedCourseExists?.semesterRegistration,
+  ).select('maxCredit');
+
+  const maxCredit = semesterRegistration?.maxCredit;
+
+  // check if total enrolled credits + new enrolled course credit exceeds max credit
+
+  const enrolledCourses = await EnrolledCourse.aggregate([
+    {
+      $match: {
+        semesterRegistration: isOfferedCourseExists?.semesterRegistration,
+        student: student._id,
+      },
+    },
+    {
+      $lookup: {
+        from: 'courses',
+        localField: 'course',
+        foreignField: '_id',
+        as: 'enrolledCoursesData',
+      },
+    },
+    {
+      $unwind: '$enrolledCoursesData',
+    },
+    {
+      $group: {
+        _id: null,
+        totalEnrolledCredits: { $sum: '$enrolledCoursesData.credits' },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        totalEnrolledCredits: 1,
+      },
+    },
+  ]);
+  const course = await Course.findById(isOfferedCourseExists?.course);
+
+  const currentCredit = course?.credits;
+
+  const totalCredits =
+    enrolledCourses?.length > 0 ? enrolledCourses[0].totalEnrolledCredits : 0;
+  console.log(totalCredits);
+
+  if (totalCredits && maxCredit && totalCredits + currentCredit > maxCredit) {
+    throw new AppError(
+      httpStatusCodes.CONFLICT,
+      'You Have Exceeded The Maximum Credit Limit',
       '',
     );
   }
