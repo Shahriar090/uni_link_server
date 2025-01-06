748ee6a84641dfbec6c6280994bfdc52f4a87636
Shahriar <shahriarh655@gmail.com>
2025-01-06 14:14:20 +0600

    Updated academic department routes. Added an auth guard before create academic department.

--- src/app/modules/academicDepartment/academicDepartment.routes.ts
+++ src/app/modules/academicDepartment/academicDepartment.routes.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { academicDepartmentValidations } from './academicDepartment.validation';
 import { academicDepartmentControllers } from './academicDepartment.controllers';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 const academicDepartmentRouter = express.Router();
 
 // create
 academicDepartmentRouter
   .route('/create-academic-department')
   .post(
+    auth(USER_ROLES.Super_Admin, USER_ROLES.Admin),
     validateRequest(
       academicDepartmentValidations.createAcademicDepartmentValidationSchema,
     ),
