bb19e0029f0a0f105338f3573636237f1625ca60
Shahriar <shahriarh655@gmail.com>
2025-01-06 14:20:00 +0600

    Updated academic semester routes. Added an auth guard before create academic semester.

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -2,11 +2,14 @@ import express from 'express';
 import { academicSemesterControllers } from './academicSemester.controllers';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { academicSemesterValidations } from './academicSemester.validation';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 const academicSemesterRouter = express.Router();
 
 academicSemesterRouter
   .route('/create-academic-semester')
   .post(
+    auth(USER_ROLES.Super_Admin, USER_ROLES.Admin),
     validateRequest(
       academicSemesterValidations.createAcademicSemesterValidationSchema,
     ),
