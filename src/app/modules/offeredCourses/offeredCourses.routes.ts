e51c7681d5768788a779cd37e7406abdbc8e90fc
Shahriar <shahriarh655@gmail.com>
2025-01-07 23:33:11 +0600

    Updated offered course routes. Implemented auth guard for create offered course route.

--- src/app/modules/offeredCourses/offeredCourses.routes.ts
+++ src/app/modules/offeredCourses/offeredCourses.routes.ts
@@ -2,12 +2,15 @@ import express from 'express';
 import { validateRequest } from '../../middlewares/validateRequest';
 import { offeredCoursesValidations } from './offeredCourses.validations';
 import { offeredCoursesControllers } from './offeredCourses.controllers';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../user/user.constant';
 const router = express.Router();
 
 // create
 router
   .route('/create-offered-course')
   .post(
+    auth(USER_ROLES.Admin, USER_ROLES.Admin),
     validateRequest(
       offeredCoursesValidations.createOfferedCoursesValidationSchema,
     ),
